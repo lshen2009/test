77a69d6f18d7eecc84dedf140370261fcc8f0195
Lulu Shen <lshen@rclogin09.rc.fas.harvard.edu>
2018-12-03 20:02:19 -0500

    lshen

--- test_subset/copy.sh
+++ test_subset/copy.sh
@@ -1,3 +1,5 @@
-cp ../github_GC_v12/Code.GC12/KPP/Standard/gckpp_JacobianSP.F90 gckpp_JacobianSP.F90
-cp ../github_GC_v12/Code.GC12/KPP/Standard/gckpp_LinearAlgebra.F90 gckpp_LinearAlgebra.F90
-cp ../github_GC_v12/Code.GC12/KPP/Standard/gckpp_Parameters.F90 gckpp_Parameters.F90
+cp /n/home03/lshen/GC_speedup/github/github_GC_v12/Code.GC12/KPP/Standard/gckpp_JacobianSP.F90 gckpp_JacobianSP.F90
+cp /n/home03/lshen/GC_speedup/github/github_GC_v12/Code.GC12/KPP/Standard/gckpp_Function.F90  gckpp_Function.F90
+cp /n/home03/lshen/GC_speedup/github/github_GC_v12/Code.GC12/KPP/Standard/gckpp_Jacobian.F90 gckpp_Jacobian.F90
+cp /n/home03/lshen/GC_speedup/github/github_GC_v12/Code.GC12/KPP/Standard/gckpp_LinearAlgebra.F90 gckpp_LinearAlgebra.F90
+cp /n/home03/lshen/GC_speedup/github/github_GC_v12/Code.GC12/KPP/Standard/gckpp_Parameters.F90 gckpp_Parameters.F90
