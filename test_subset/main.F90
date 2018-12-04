b0849f3823861891c1afbde8224f5325cff095c1
Lu Shen <lshen729@gmail.com>
2018-12-03 20:39:50 -0500

    lshen

--- test_subset/main.F90
+++ test_subset/main.F90
@@ -11,7 +11,7 @@ program main
   REAL(kind=dp) :: JVS(LU_NONZERO_12),JVS_orig(LU_NONZERO_12)
   REAL(kind=dp) :: X_selected(LU_NSEL_12),X_deleted(LU_NDEL_12),X(NVAR)
   REAL(kind=dp) :: JVS1(LU_NONZERO_12),JVS2(LU_NONZERO_12)
-  REAL(kind=dp) :: F(NFIX),RCT(NREACT),Vdot(LU_NSEL_2)
+  REAL(kind=dp) :: F(NFIX),RCT(NREACT),Vdot(LU_NSEL_12)
   INTEGER:: IER,i,j,k
 
   CALL initialize_1D(JVS_orig)
