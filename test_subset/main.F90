447bea5f2388960ca4d3e6d1e55bf5751bddad02
Lu Shen <lshen729@gmail.com>
2018-12-03 20:20:44 -0500

    lshen

--- test_subset/main.F90
+++ test_subset/main.F90
@@ -8,7 +8,7 @@ program main
   
 
   REAL(kind=dp) :: JVS(LU_NONZERO_12),JVS_orig(LU_NONZERO_12)
-  REAL(kind=dp) :: VAR_Selected(LU_NSEL_12),VAR_Selected(LU_NSEL_12),X(NVAR)
+  REAL(kind=dp) :: VAR_Selected(LU_NSEL_12),VAR_Selected(LU_NDEL_12),X(NVAR)
   REAL(kind=dp) :: JVS1(LU_NONZERO_12),JVS2(LU_NONZERO_12)
   INTEGER:: IER,i,j,k
 
