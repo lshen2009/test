1980785eef09875440af73e891874f528ac2e927
Lu Shen <lshen729@gmail.com>
2018-12-03 20:44:57 -0500

    lshen

--- test_subset/main.F90
+++ test_subset/main.F90
@@ -11,7 +11,7 @@ program main
   REAL(kind=dp) :: JVS(LU_NONZERO_12),JVS_orig(LU_NONZERO_12)
   REAL(kind=dp) :: X_selected(LU_NSEL_12),X_deleted(LU_NDEL_12),X(NVAR)
   REAL(kind=dp) :: JVS1(LU_NONZERO_12),JVS2(LU_NONZERO_12)
-  REAL(kind=dp) :: F(NFIX),RCT(NREACT),Vdot(LU_NSEL_12)
+  REAL(kind=dp) :: F(NFIX),RCT(NREACT),Vdot_SEL(LU_NSEL_12),Vdot(NVAR)
   INTEGER:: IER,i,j,k
 
   CALL initialize_1D(JVS_orig)
@@ -19,11 +19,12 @@ program main
   CALL initialize_1D(F)
   CALL initialize_1D(RCT)
   Vdot=0
+  Vdot_SEL=0
   
   X_selected=X(select_ind_12)
   X_deleted=X(delete_ind_12)
   !First check the Function
-  CALL Fun_12 ( X_selected,X_deleted, F, RCT, Vdot)
+  CALL Fun_12 ( X_selected,X_deleted, F, RCT, Vdot_SEL)
   CALL Fun_13 ( X, F, RCT, Vdot, NVAR )
   
 
